164d1669c4d33afe0f45e772d0a74e105261658a
paulazoo <paulazhu@college.harvard.edu>
2021-04-05 08:34:12 -0400

    oops add applicant status to mentor params

--- app/controllers/mentor_applicants_controller.rb
+++ app/controllers/mentor_applicants_controller.rb
@@ -97,6 +97,6 @@ class MentorApplicantsController < ApplicationController
       :school, :essay, \
       :first_gen, :low_income, :stem_girl, :single_parent, :disabled, :lgbt, \
       :black, :hispanic, :asian, :pi, :me_na, :native, :immigrant, :undoc, \
-      :password ])
+      :password, :applicant_status ])
   end
 end
